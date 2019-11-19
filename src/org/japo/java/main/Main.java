fe233c7fe6b6b3bdd23719757ff7b8c60700c6b3
malabo1990 <malabo1990@gmail.com>
2019-11-19 10:26:24 +0100

    Add files via upload

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.japo.java.main;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Casimiro Ondo Obiang casimiro.ondo.alum@iescamp.es
+ */
+public class Main {
+    public static void main(String[] args) {
+        //ejercicio-CuentaProgresivaFor
+        //Constante
+        final int MIN = 0;
+        final int MAX = 9;
+        //variable
+       //bucle
+       for(int i=MIN;i<=MAX;i++)
+        System.out.println(i);
+    }
+}
